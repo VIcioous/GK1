a7dfbc2ce0e1d661f73ffe97a38da508a9c81a64
daniel.zywalewski <daniel.zywalewski@britenet.com.pl>
2021-10-13 07:22:49 +0200

    Initial commit

---
+++ src/com/company/Canvas.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Canvas extends JFrame {
+
+    Canvas()
+    {
+        this.setTitle("Paint - Grafika Komputerowa");
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
